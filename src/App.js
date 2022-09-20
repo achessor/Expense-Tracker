dd9e9d6a4c551aea00328466641762e2a503ccb2
Alex <alex.chessor@gmail.com>
2022-09-20 00:39:32 -0500

    Updated expense tracker
    added more components to tracker. 
    added forms to create new expenses

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense';
 
-function App() {
+const App = () => {
   const expenses = [
-    { title: 'Car Insurance', amount: 105.16, date: new Date(2022, 9, 11) },
-    { title: 'Mortgage', amount: 1800.0, date: new Date(2022, 9, 15) },
-    { title: 'Cell Phone', amount: 215.5, date: new Date(2022, 9, 8) },
-    { title: 'Storage', amount: 220.0, date: new Date(2022, 9, 10) },
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 105.16,
+      date: new Date(2022, 9, 11),
+    },
+    {
+      id: 'e2',
+      title: 'Mortgage',
+      amount: 1800.0,
+      date: new Date(2022, 9, 15),
+    },
+    {
+      id: 'e3',
+      title: 'Cell Phone',
+      amount: 215.5,
+      date: new Date(2022, 9, 8),
+    },
+    { id: 'e4', title: 'Storage', amount: 220.0, date: new Date(2022, 9, 10) },
   ];
 
   return (
     <div>
-      <h2>My Expenses!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
